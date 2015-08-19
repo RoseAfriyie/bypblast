33ff4a3db06bb7a9e9a5f8f947895e22a8c73637
Ellie Day <heyellieday@gmail.com>
2015-08-18 19:34:07 -0500

    Fix has_many responses, through line in Event model

--- app/models/blast.rb
+++ app/models/blast.rb
@@ -1,3 +1,4 @@
 class Blast < ActiveRecord::Base
 	belongs_to :ownable, polymorphic: true
+	has_many :responses
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
 	has_many :blasts, as: :ownable
-	has_many :events, through: :blasts
+	has_many :responses, through: :blasts
 end
