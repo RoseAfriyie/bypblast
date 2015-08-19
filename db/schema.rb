1f335cef8ca2846413e1ec41f569f61fd3eb1259
Ellie Day <heyellieday@gmail.com>
2015-08-18 19:35:42 -0500

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819001653) do
+ActiveRecord::Schema.define(version: 20150819002542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "blasts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "ownable_id"
+    t.string   "ownable_type"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "blasts", ["ownable_type", "ownable_id"], name: "index_blasts_on_ownable_type_and_ownable_id", using: :btree
 
   create_table "contacts", force: :cascade do |t|
     t.hstore   "details",    default: {}, null: false
@@ -33,6 +43,14 @@ ActiveRecord::Schema.define(version: 20150819001653) do
 
   add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id", using: :btree
   add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id", using: :btree
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.integer  "conversation_id"
@@ -52,5 +70,13 @@ ActiveRecord::Schema.define(version: 20150819001653) do
   end
 
   add_index "numbers", ["ownable_type", "ownable_id"], name: "index_numbers_on_ownable_type_and_ownable_id", using: :btree
+
+  create_table "responses", force: :cascade do |t|
+    t.string   "value"
+    t.integer  "number_id"
+    t.integer  "blast_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
